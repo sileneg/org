6aadc23d52d9a3d9983dcf91c88028299f31f0ec
sileneg <111543386+sileneg@users.noreply.github.com>
2024-12-22 01:18:27 -0500

    Update Header.js

--- org/src/componentes/Header/Header.js
+++ org/src/componentes/Header/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css"
 function Header(){
     return <header className="header">
-        <img src="/img/header.png" alt ='Org' />
+        <img src="org/img/header.png" alt ='Org' />
     </header>
 }
 
